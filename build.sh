9223b30ea2c4fe13b9792b2d072a3a9445084e21
MaxRegner <regnermax8@gmail.com>
2022-10-14 09:21:16 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -141,55 +141,6 @@ DATE=$(TZ=Asia/Kolkata date +"%Y-%m-%d")
 	fi
 }
 
-
-}
-
-##---------------------------------------------------------##
-
-tg_post_build() {
-	#Post MD5Checksum alongwith for easeness
-	MD5CHECK=$(md5sum "$1" | cut -d' ' -f1)
-
-	#Show the Checksum alongwith caption
-	curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
-	-F chat_id="$CHATID"  \
-	-F "disable_web_page_preview=true" \
-	-F "parse_mode=html" \
-	-F caption="$2 | <b>MD5 Checksum : </b><code>$MD5CHECK</code>"
-}
-
-##----------------------------------------------------------##
-
-tg_send_sticker() {
-    curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendSticker" \
-        -d sticker="$1" \
-        -d chat_id="$CHATID"
-}
-
-##----------------------------------------------------------------##
-
-tg_send_files(){
-    KernelFiles="$(pwd)/$KERNELNAME.zip"
-	MD5CHECK=$(md5sum "$KernelFiles" | cut -d' ' -f1)
-	SID="CAACAgUAAxkBAAIlv2DEzB-BSFWNyXkkz1NNNOp_pm2nAAIaAgACXGo4VcNVF3RY1YS8HwQ"
-	STICK="CAACAgUAAxkBAAIlwGDEzB_igWdjj3WLj1IPro2ONbYUAAIrAgACHcUZVo23oC09VtdaHwQ"
-    MSG="✅ <b>Build Done</b>
-- <code>$((DIFF / 60)) minute(s) $((DIFF % 60)) second(s) </code>
-<b>Build Type</b>
--<code>$BUILD_TYPE</code>
-<b>MD5 Checksum</b>
-- <code>$MD5CHECK</code>
-<b>Zip Name</b>
-- <code>$KERNELNAME.zip</code>"
-
-        curl --progress-bar -F document=@"$KernelFiles" "https://api.telegram.org/bot$TOKEN/sendDocument" \
-        -F chat_id="$CHATID"  \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="$MSG"
-
-}
-
 ##----------------------------------------------------------##
 
 build_kernel() {
